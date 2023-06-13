7203ff29e8f7979846bb3c2bbb7b046e5f3eca71
yangbiaoli <yangbiaoli@trip.com>
2023-06-13 20:36:15 +0800

    增加自适应学习率调整；

--- learn/demo2/train.py
+++ learn/demo2/train.py
@@ -7,11 +7,12 @@ from learn.demo2.myutils import getAccuracy, showImgArray
 from loss import ContrastiveLoss
 from net import SiameseNetwork
 import torch.optim as optim
-from loadData import getDataloder,getTestDataloder
+from loadData import getDataloder, getTestDataloder
 from test import make_test
 from tqdm import tqdm
 
 from utils.callbacks import LossHistory
+from utils.utils import get_lr_scheduler, set_optimizer_lr
 
 train_number_epochs = 10
 train_batch_size = 64
@@ -21,7 +22,6 @@ testdataloder = getTestDataloder(batch_size=train_batch_size)
 
 net = SiameseNetwork()
 loss_func = ContrastiveLoss()
-optimizer = optim.Adam(net.parameters(),lr = 0.0005)
 
 # ---------------------------------------#
 #   判断每一个世代的长度
@@ -31,14 +31,34 @@ num_val = len(testdataloder.dataset)
 epoch_step = num_train // train_batch_size
 epoch_step_val = num_val // train_batch_size
 
-
+# ---------------------------------------#
+#   获得学习率下降的公式
+# ---------------------------------------#
+optimizer_type = "adam"
+lr_decay_type = 'cos'
+nbs = 64
+Init_lr = 1e-2
+Min_lr = Init_lr * 0.01
+lr_limit_max = 1e-3 if optimizer_type == 'adam' else 1e-1
+lr_limit_min = 3e-4 if optimizer_type == 'adam' else 5e-4
+Init_lr_fit = min(max(train_batch_size / nbs * Init_lr, lr_limit_min), lr_limit_max)
+Min_lr_fit = min(max(train_batch_size / nbs * Min_lr, lr_limit_min * 1e-2), lr_limit_max * 1e-2)
+momentum = 0.9
+weight_decay = 5e-4
+optimizer = {
+    'adam': optim.Adam(net.parameters(), Init_lr_fit, betas=(momentum, 0.999), weight_decay=weight_decay),
+    'sgd': optim.SGD(net.parameters(), Init_lr_fit, momentum=momentum, nesterov=True, weight_decay=weight_decay)
+}[optimizer_type]
+lr_scheduler_func = get_lr_scheduler(lr_decay_type, Init_lr_fit, Min_lr_fit, train_number_epochs)
 
 if __name__ == '__main__':
     os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
     loss_history = LossHistory(save_dir, net, input_shape=input_shape)
     for epoch in range(train_number_epochs):
 
-        total =0
+        set_optimizer_lr(optimizer, lr_scheduler_func, epoch)
+
+        total = 0
         total_loss = 0
         total_accuracy = 0
 
@@ -52,7 +72,8 @@ if __name__ == '__main__':
         for i, data in enumerate(dataloader):
             # img0,img1  (batch, 3, 32, 32)
             # label (batch)
-            img_1, img_2, label = data[0].type(torch.FloatTensor), data[1].type(torch.FloatTensor), data[2].type(torch.FloatTensor)
+            img_1, img_2, label = data[0].type(torch.FloatTensor), data[1].type(torch.FloatTensor), data[2].type(
+                torch.FloatTensor)
             # showImgArray(img_1, img_2, label)
             optimizer.zero_grad()
             output_1, output_2 = net(img_1, img_2)
@@ -73,10 +94,12 @@ if __name__ == '__main__':
         pbar.close()
         print('Finish Train')
         print('Start Validation')
-        pbar = tqdm(total=epoch_step_val, desc=f'Epoch {epoch + 1}/{train_number_epochs}', postfix=dict, mininterval=0.3)
+        pbar = tqdm(total=epoch_step_val, desc=f'Epoch {epoch + 1}/{train_number_epochs}', postfix=dict,
+                    mininterval=0.3)
 
         for i, data in enumerate(testdataloder):
-            img_1, img_2, label = data[0].type(torch.FloatTensor), data[1].type(torch.FloatTensor), data[2].type(torch.FloatTensor)
+            img_1, img_2, label = data[0].type(torch.FloatTensor), data[1].type(torch.FloatTensor), data[2].type(
+                torch.FloatTensor)
             optimizer.zero_grad()
             output_1, output_2 = net(img_1, img_2)
             loss = loss_func(output_1, output_2, label)
@@ -103,12 +126,10 @@ if __name__ == '__main__':
         # -----------------------------------------------#
         if (epoch + 1) % save_period == 0 or epoch + 1 == train_number_epochs:
             torch.save(net.state_dict(), os.path.join(save_dir, "ep%03d-loss%.3f-val_loss%.3f.pth" % (
-            epoch + 1, total_loss / epoch_step, val_loss / epoch_step_val)))
+                epoch + 1, total_loss / epoch_step, val_loss / epoch_step_val)))
 
         if len(loss_history.val_loss) <= 1 or (val_loss / epoch_step_val) <= min(loss_history.val_loss):
             print('Save best model to best_epoch_weights.pth')
             torch.save(net.state_dict(), os.path.join(save_dir, "best_epoch_weights.pth"))
 
         torch.save(net.state_dict(), os.path.join(save_dir, "last_epoch_weights.pth"))
-
-
--- utils/utils_aug.py
+++ utils/utils_aug.py
@@ -157,7 +157,7 @@ class SubPolicy(object):
             "shearY": np.linspace(0, 0.3, 10),
             "translateX": np.linspace(0, 150 / 331, 10),
             "translateY": np.linspace(0, 150 / 331, 10),
-            "rotate": np.linspace(0, 30, 10),
+            "rotate": np.linspace(0, 15, 10),
             "color": np.linspace(0.0, 0.9, 10),
             "posterize": np.round(np.linspace(8, 4, 10), 0).astype(np.int),
             "solarize": np.linspace(256, 0, 10),
